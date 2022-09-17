7f20e77a2b6d2caeb264b45de9d77401343c69e1
JuravekJr <jurabek009developer@gmail.com>
2022-09-17 20:34:28 +0500

    Echo-Bot

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser')
+
+const { TOKEN, SERVER_URL } = process.env
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
+const URI = `/webhook/${TOKEN}`
+const WEBHOOK_URL = SERVER_URL + URI
+
+const app = express()
+app.use(bodyParser.json())
+
+const init = async () => {
+    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
+    console.log(res.data)
+}
+
+app.post(URI, async (req, res) => {
+    console.log(req.body)
+
+    const message = {
+        chat_id: req.body.message.chat.id,
+        text: req.body.message.text
+    }
+
+    await axios.post(`${TELEGRAM_API}/sendMessage`, message)
+    return res.send()
+})
+
+app.listen(process.env.PORT || 5000, async () => {
+    console.log('🚀 app running on port', process.env.PORT || 5000)
+    await init()
+})
